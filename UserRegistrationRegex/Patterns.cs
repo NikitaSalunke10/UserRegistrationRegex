1860f71dacd6eef2b7128ace6d976ebe81e7915a
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-02 19:40:27 +0530

    First name validation

--- UserRegistrationRegex/Patterns.cs
+++ UserRegistrationRegex/Patterns.cs
@@ -10,13 +10,9 @@ namespace UserRegistrationRegex
     internal class Patterns
     {
         public static string RegexRules = "^[A-Z][a-z]{3,}$"; 
-        public bool validateString(string sample) //this method is used to check whether the input from user match with the regex rules written
-        {
-            return Regex.IsMatch(sample, RegexRules);
-        }
         public void validateFName(string FName)//this method is to check if First name is valid or not
         {
-            if(validateString(FName))
+            if(Regex.IsMatch(FName,RegexRules))
             {
                 Console.WriteLine("First Name: " + FName);
             }
