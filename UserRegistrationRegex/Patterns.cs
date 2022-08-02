4b8d0ffca1c872f78ebba3097ce75235ac1f2649
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-02 19:30:26 +0530

    Add project files.

---
+++ UserRegistrationRegex/Patterns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    internal class Patterns
+    {
+        public static string RegexRules = "^[A-Z][a-z]{3,}$";
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+        public void validateFName(string FName)
+        {
+            if(validateString(FName))
+            {
+                Console.WriteLine("First Name: " + FName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+        }
+    }
+}
---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,6 @@
+﻿using UserRegistrationRegex;
+
+Patterns pattern = new Patterns();
+Console.WriteLine("Enter the First Name: ");
+string FName = Console.ReadLine();
+pattern.validateFName(FName);
