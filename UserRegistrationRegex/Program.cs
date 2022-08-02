e8b2f4a9ced99e48161d0ad6ef312b40d45142bf
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-02 19:48:52 +0530

    Last Name validation

--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -3,11 +3,7 @@
 Patterns pattern = new Patterns();
 Console.WriteLine("Enter the First Name: ");
 string FName = Console.ReadLine();
-<<<<<<< HEAD
-pattern.validateFName(FName);
-=======
 Console.WriteLine("Enter the Last Name: ");
 string LName = Console.ReadLine();
 pattern.validateFName(FName);
 pattern.validateLName(LName);
->>>>>>> UC-2-ValidLastName
